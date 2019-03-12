c542c063eaa4816b9044287a2da97e9cbadb7450
lee <xinghlee@163.com>
2019-03-12 16:37:00 +0800

    添加日志配置

--- src/test/java/com/lee/my_springboot/MySpringbootApplicationTests.java
+++ src/test/java/com/lee/my_springboot/MySpringbootApplicationTests.java
@@ -1,16 +1,27 @@
 package com.lee.my_springboot;
 
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MySpringbootApplicationTests {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+//    protected final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void contextLoads() {
+        logger.trace("I am trace log.");
+        logger.debug("I am debug log.");
+        logger.warn("I am warn log.");
+        logger.error("I am error log.");
+        logger.info("-----------------------------------------------------------------");
+        logger.trace("******************************************************************");
     }
 
 }
